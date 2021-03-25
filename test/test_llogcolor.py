a63a9d42a77fe4f155b71adb88cf95c256a35061
Gian-Carlo DeFazio <defazio1@llnl.gov>
2021-03-25 13:51:49 -0700

    Ported llogcolor to python3 and added tests
    llogcolor has been ported from python2 to python3.
    
    The optparse library (which is deprecated)
    has been replaced with the argparse library.
    
    The function next_ansi_color() was removed because it
    modifies global state and this is a problem for tests
    that expect outputs to match verbatim. The functionality
    of next_ansi_color() is now in main().
    
    Many print statements were changed to use string formatting.
    
    The pathlib library is being used in place of the os library
    for file and directory operations.
    
    The parser is built in a separate function.
    
    main() is modified so as to allow arguments not just from the
    command line. This is for testing purposes.
    
    The various outputs (terminal, less, files) are all set
    up to accept strings.
    
    There is now a test directory, which contains a test for
    llogcolor. It also has reference files for the test outputs.
    The test can be run directly with python:
    >$ python test_llogcolor.py

---
+++ test/test_llogcolor.py
@@ -0,0 +1,110 @@
+'''
+A test suite for comparing the text output
+of llogcolor for an assumed-to-be-correct output.
+
+The pager is skipped for these tests, and the
+color control codes go strait to a file, so they get saved.
+
+There are a bunch of input, and corresponding output files.
+'''
+
+import filecmp
+import pathlib
+import sys
+import tempfile
+import unittest
+
+# now I need to import llogcolor
+# this isn't the normal way to import,
+# but there are 2 issues.
+# 1. this is not a python only project, I don't want
+# to structure all of lustre-tools-llnl as python package
+# 2. the file to be tested doesn't have '.py' suffix
+# from https://stackoverflow.com/questions/2601047/
+from importlib.util import spec_from_loader, module_from_spec
+from importlib.machinery import SourceFileLoader
+
+path_to_llogcolor = str(pathlib.Path('../scripts/llogcolor').resolve())
+spec = spec_from_loader(
+    'llogcolor',
+    SourceFileLoader('llogcolor', path_to_llogcolor)
+)
+llogcolor = module_from_spec(spec)
+spec.loader.exec_module(llogcolor)
+
+
+
+
+#### paths to input logs ###
+# small and simple log file, all of the same thread ID
+in_simple = 'llogcolor_files/sample_logs'
+
+### paths to output logs ###
+# [-P, -C, in_simple]
+out_P_C_simple = 'llogcolor_files/sample_logs'
+# [-C, in_simple]
+out_P_simple = 'llogcolor_files/out_simple_color'
+
+
+def cmp_stdout_to_ref(test_args, reference):
+    '''Compare stdout to reference file.
+    run llogcolor with test_args and write
+    the output to a temporary file.
+    Then compare that file to a reference file.
+    Return True if the files match and False otherwise.
+    '''
+    # stdout will be changed to the temporary file
+    # for when llogcolor outputs stuff
+    # so save the real stdout now
+    real_stdout = sys.stdout
+
+    # create a temporary file
+    # and set aim stdout at it
+    fd, filename = tempfile.mkstemp()
+    f = open(filename, 'w')
+    sys.stdout = f
+
+    # run llogcolor with test_args and save the return value
+    ret_val = llogcolor.main(args=test_args)
+
+    # put stdout back to normal
+    sys.stdout = real_stdout
+    print(ret_val)
+    # not sure if this is neccesary, but want it
+    # it all written befor the compare
+    f.close()
+
+    files_match = filecmp.cmp(reference, filename)
+    print(files_match)
+    #f.close()
+
+    success = ((ret_val is None) or (ret_val == 0)) and files_match
+    return success
+
+
+class TestTextOutput(unittest.TestCase):
+    '''Tests that create output and then compare
+    that output to a reference file.
+    '''
+
+    def test_P_C_simple(self):
+        '''read a log file in, and just send it unmodified to stdout'''
+        # no pager, no color
+        test_args = ['-P', '-C', in_simple]
+        reference = out_P_C_simple
+
+        self.assertTrue(cmp_stdout_to_ref(test_args, reference))
+
+    def test_P_simple(self):
+        '''read a log file in, and send the text to stdout.
+        In this case the text includes the terminal color codes
+        In this case all the lines will have the same color code.
+        '''
+        # no pager
+        test_args = ['-P', in_simple]
+        reference = out_P_simple
+
+        self.assertTrue(cmp_stdout_to_ref(test_args, reference))
+
+if __name__ == '__main__':
+    unittest.main()
